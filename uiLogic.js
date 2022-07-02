2a85ea01e8834f773ace16f2071c5cabaea07308
kevfo <75442525+kevfo@users.noreply.github.com>
2022-07-02 22:28:24 +0800

    Finished first draft of Knockout.JS viewModel function

--- uiLogic.js
+++ uiLogic.js
@@ -1,30 +1,35 @@
 // All of our user's expenditures.
-var expenses = [{name: 'Mars Bar', quantity: 1, price: 0.5}]
+var expenses = [
+  {itemName: 'Mars Bar', itemQuantity: '0', itemPrice: '0'}
+]
 
-var viewModel = function(name, quantity, price, money) {
+var viewModel = function(expense, money) {
   var self = this;
-  self.expenditures = ko.observableArray(expenses);
+  self.expenditures = ko.observableArray(expense);
 
   // The user can mess around with these variables:
-  self.itemName = ko.observable(name);
-  self.itemQuantity = ko.observable(quantity);
-  self.itemPrice = ko.observable(price);
   self.totalMoney = ko.observable(money);
 
   // Button functionalities:
   self.addItemToTable = function() {
     self.expenditures.push(
       {
-        name : self.itemName(),
-        quantity: self.itemQuantity(),
-        price: self.itemPrice()
+        itemName : '',
+        itemQuantity: '',
+        itemPrice: ''
       }
     )
   }
 
-  self.sortByNames = function() {
+  self.sortByPrice = function() {
     self.expenditures.sort(function(a, b) {
-      return a.name < b.name ? -1 : 1;
+      return a.itemPrice < b.itemPrice ? -1 : 1;
+    });
+  }
+
+  self.sortByQuantity = function() {
+    self.expenditures.sort(function(a, b) {
+      return a.itemQuantity < b.itemQuantity ? -1 : 1;
     });
   }
 
@@ -33,36 +38,29 @@ var viewModel = function(name, quantity, price, money) {
   }
 
   // Summary statistics to let the user know how good / bad they are...
+  self.leftOver = ko.computed(function() {
+    let initial = self.totalMoney();
+    self.expenditures().forEach(item => {
+      initial -= parseInt(item.itemPrice);
+    })
+    return initial > 0 ? `$${initial.toFixed(2)}` : '0$';
+  }, self);
 
-  self.tallyStats = function() {
-    self.leftOver = ko.computed(function() {
-      let initial = self.totalMoney();
-      self.expenditures().forEach(item => {
-        initial -= item.price;
-      })
-      return initial >= 0 ? initial.toFixed(2) : 0;
-    }, self);
+  self.moneyGone = ko.computed(function() {
+    let totalSpent = 0;
+    self.expenditures().forEach(item => {
+      totalSpent += parseInt(item.itemPrice) * parseInt(item.itemQuantity);
+    });
+    return `$${totalSpent}`;
+  }, self);
 
-    self.moneySpent = ko.computed(function() {
-      let totalSpent = 0;
-      self.expenditures().forEach(item => {
-        totalSpent += item.price;
-      });
-      return totalSpent;
-    }, self);
+  self.itemsBought = ko.computed(function() {
+    let itemCount = 0;
+    self.expenditures().forEach(item => {itemCount++});
+    return itemCount + ' items';
+  }, self);
 
-    self.itemsBought = ko.computed(function() {
-      let itemCount = 0;
-      self.expenditures().forEach(item => {itemCount++});
-      return itemCount;
-    }, self);
-
-    return {first: self.leftOver, second: self.moneySpent, third: self.itemsBought};
-  }
-
-  let summaryStats = self.tallyStats();
-  self.leftOver = summaryStats.first;
-  self.moneySpent = summaryStats.second;
-  self.itemsBought = summaryStats.third;
-  
+  self.finalConsensus = ko.computed(function() {
+    return self.moneyGone > self.totalMoney() ? "Yes :(" : "No :)";
+  })
 }
